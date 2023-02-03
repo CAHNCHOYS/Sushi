afc46c2a04b544a8a87efd8912cac3d83d1941c4
SAHNCOYS <lullullullul94@gamil.com>
2023-02-03 20:30:16 +0300

    first commit

---
+++ assets/CartView-f9a2d6d6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as t,d as _,b as s,U as c}from"./index-6584aca8.js";const o={class:"cart-page"},r={class:"cart-page__body"},d={__name:"CartView",setup(n){return(p,i)=>(a(),t("div",o,[_("div",r,[s(c)])]))}},u=e(d,[["__scopeId","data-v-7d5a45a0"]]);export{u as default};
